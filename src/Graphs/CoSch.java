686b877f3bf706b95727968af1b98fdf8860ab6b
Varad Jadhav <142712416+VaradJadhav37@users.noreply.github.com>
2025-06-22 23:38:11 +0530

    cosch

---
+++ src/Graphs/CoSch.java
@@ -0,0 +1,44 @@
+package Graphs;
+
+public class CoSch {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        int[] indegree = new int[numCourses];
+        for (int[] edge : prerequisites) {
+            int course = edge[0];
+            int prereq = edge[1];
+            adj.get(prereq).add(course);
+            indegree[course]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        int[] arr=new int[numCourses];
+        int count = 0;
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            arr[count]=curr;
+            count++;
+
+            for (int neighbor : adj.get(curr)) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.add(neighbor);
+                }
+            }
+        }
+        int c=0;
+        if (count == numCourses) {
+            return arr;
+        } else {
+            return new int[0];
+        }
+    }
+}
