171a5f508c06d24fd7642c4b0d001e196319a030
Varad Jadhav <142712416+VaradJadhav37@users.noreply.github.com>
2025-07-11 23:53:59 +0530

    bin5

---
+++ src/Arrays/BinSearch5.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class BinSearch5 {
+    public int singleNonDuplicate(int[] nums) {
+        int h=nums.length-1;
+        if(h==0) return nums[0];
+        if(nums[h]!=nums[h-1])return nums[h];
+        if(nums[0]!=nums[1])return nums[0];
+        int l=1;
+        h--;
+        int mid=(h+l)/2;
+        while(l<=h){
+            mid=(h+l)/2;
+            if(nums[mid-1]!=nums[mid] && nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }
+            else if((nums[mid-1]==nums[mid] && mid%2==0)
+                    ||
+                    (nums[mid+1]==nums[mid] && mid%2==1) ){
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+
+        }
+        return -1;
+    }
+}
