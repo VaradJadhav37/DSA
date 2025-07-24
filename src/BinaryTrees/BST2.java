4cee72970f8cdb9121b6cfadefc04fc3483fc406
Varad Jadhav <142712416+VaradJadhav37@users.noreply.github.com>
2025-07-24 23:01:35 +0530

    bst2

---
+++ src/BinaryTrees/BST2.java
@@ -0,0 +1,23 @@
+package BinaryTrees;
+/**
+ * Definition for a binary tree node.
+        * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class BST2{
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null)return null;
+        if(p.val<root.val && q.val<root.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if(p.val>root.val && q.val>root.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+
+    }
+}
